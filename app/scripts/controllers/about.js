95340a4652f1a65cabe2ef2e069da7e9111a0877
qetennyson <quincytennyson@yahoo.com>
2016-04-05 22:09:22 -0400

    second yeoman commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightningtalktestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lightningtalktestApp
+ */
+angular.module('lightningtalktestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightningtalktestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lightningtalktestApp
+ */
+angular.module('lightningtalktestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
